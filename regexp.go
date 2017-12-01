24607884b3e47637c71a031069509b54d4eff2cd
g00339839 <g00339839@gmit.ie>
2017-12-01 18:03:16 +0000

    Reflect pronouns

--- regexp.go
+++ regexp.go
@@ -4,8 +4,27 @@ import (
 	"fmt"
 	"math/rand"
 	"regexp"
+	"strings"
 	"time"
 )
+
+var reflections = [][]string{
+	{"am", "are"},
+	{"was", "were"},
+	{"i", "you"},
+	{"i'd", "you would"},
+	{"i've", "you have"},
+	{"i'll", "you will"},
+	{"my", "your"},
+	{"are", "am"},
+	{"you've", "I have"},
+	{"you'll", "I will"},
+	{"your", "my"},
+	{"yours", "mine"},
+	{"you", "i"},
+	{"me", "you"},
+	{"myself", "yourself"},
+	{"you're", "i'am"}}
 
 func elizaResponse(input string) string {
 
@@ -16,11 +35,35 @@ func elizaResponse(input string) string {
 
 		return "Why don’t you tell me more about your father?"
 
-	} else if matched := reg.MatchString(input); matched {
+	}
 
-		replace := reg.ReplaceAllString(input, "How do you know you are $2?")
+	if matched := reg.MatchString(input); matched {
 
-		return replace
+		converted := reg.ReplaceAllString(input, "$2")
+
+		// Split the input on word boundaries.
+		boundaries := regexp.MustCompile(`\b `)
+		tokens := boundaries.Split(converted, -1)
+
+		// Loop through each token, reflecting it if there's a match.
+		for i, token := range tokens {
+
+			for _, reflection := range reflections {
+
+				matched := strings.Compare(strings.Trim(token, `$(.!)?`), reflection[0])
+
+				if matched == 0 {
+
+					tokens[i] = reflection[1]
+
+				}
+			}
+		}
+
+		// Put the tokens back together.
+		converted = strings.Join(tokens, ` `)
+
+		return "How do you know you are " + converted
 	}
 
 	responses := []string{"I’m not sure what you’re trying to say. Could you explain it to me?", "How does that make you feel?", "Why do you say that?"}
@@ -52,12 +95,20 @@ func main() {
 	fmt.Println(elizaResponse("I am not happy with your responses."))
 	fmt.Println("I am not sure that you understand the effect that your questions are having on me.")
 	fmt.Println(elizaResponse("I am not sure that you understand the effect that your questions are having on me."))
-	fmt.Println("I am supposed to just take what you’re saying at face value?")
-	fmt.Println(elizaResponse("I am supposed to just take what you’re saying at face value?"))
+	fmt.Println("I am supposed to just take what you're saying at face value?")
+	fmt.Println(elizaResponse("I am supposed to just take what you're saying at face value?"))
 
 	fmt.Println("------------------------------------------------")
-	fmt.Println("I'm so happy")
-	fmt.Println(elizaResponse("I'm so happy"))
-	fmt.Println("Im so happy")
-	fmt.Println(elizaResponse("Im so happy"))
+	fmt.Println("I'm happy.")
+	fmt.Println(elizaResponse("I'm happy."))
+	fmt.Println("Im not happy with your responses.")
+	fmt.Println(elizaResponse("Im not happy with your responses."))
+	fmt.Println("i'm not sure that you understand the effect that your questions are having on me.")
+	fmt.Println(elizaResponse("i'm not sure that you understand the effect that your questions are having on me."))
+	fmt.Println("I AM supposed to just take what you're saying at face value?")
+	fmt.Println(elizaResponse("I AM supposed to just take what you're saying at face value?"))
+	fmt.Println("------------------------------------------------")
+	fmt.Println("I am not sure that you understand the effect your questions are having on me.")
+	fmt.Println(elizaResponse("I am not sure that you understand the effect your questions are having on me."))
+
 }
